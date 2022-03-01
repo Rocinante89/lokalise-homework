98258867cf624e80562321f035e07bdf8d12fc2a
Cillian Barron <barroncillian@gmail.com>
2022-03-01 14:13:25 +0000

    refactor: :art: remove superfluous divs

--- src/features/filteredGrid/FilteredGrid.tsx
+++ src/features/filteredGrid/FilteredGrid.tsx
@@ -1,11 +1,7 @@
 import * as React from 'react';
-import { SearchInput } from '../../shared/ui';
 
 const FilteredGrid: React.FunctionComponent = () => {
-    return <div className='flex flex-col gap-y-4 sm:gap-y-6 lg:gap-y-8'>
-        <SearchInput />
-        <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3'>
-        </div>
+    return <div className='grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3'>
     </div>;
 };
 
