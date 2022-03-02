138f236b24e019ee512a952890e1fa7f603a4513
Cillian Barron <barroncillian@gmail.com>
2022-03-02 21:01:51 +0000

    feat: :lipstick: Apply lokalise theme

--- src/shared/ui/card/Card.tsx
+++ src/shared/ui/card/Card.tsx
@@ -18,15 +18,15 @@ interface ICardOptionsProps {
 
 export const Card: React.FunctionComponent<ICardProps> = ({ date, description, title, userName, options }) => {
     return <>
-        <div className="w-full h-full rounded bg-gray-100 flex flex-col justify-start gap-y-2 p-4">
-            <h2 className="text-blue-600 text-xl font-medium">{title}</h2>
-            <span className='grow text-sm'>{description}</span>
+        <div className="w-full h-full rounded bg-lokalise-gray-light flex flex-col justify-start gap-y-2 p-4">
+            <h2 className="text-lokalise-blue-primary text-xl font-medium">{title}</h2>
+            <p className='grow text-sm text-lokalise-content-secondary'>{description}</p>
             <div className='flex justify-start gap-1'>
                 <Badge colour='gray' label={`${options.translatable ? "" : "Not-"}translatable`} />
                 <Badge colour='blue' label={`Case-${options.caseSensitive ? "sensitive" : "insensitive"}`} />
                 <Badge colour='red' label={`${options.forbidden ? "" : "Not-"}forbidden`} />
             </div>
-            <span className='text-xs'>{format(new Date(date), "dd MMM',' yyyy")} • {userName}</span>
+            <span className='text-xs text-lokalise-content-secondary'>{format(new Date(date), "dd MMM',' yyyy")} • {userName}</span>
         </div>
     </>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'lokalise-red': '#E36A77',
+      'lokalise-blue': '#78A4F6',
+      'lokalise-blue-primary': '#5489DC',
+      'lokalise-gray': '#BBBBBB',
+      'lokalise-gray-light': '#F9F9F9',
+      'lokalise-content-secondary': '#808080',
+      'white': '#ffffff',
+    },
+    fontFamily: {
+      sans: ['roboto'],
+      serif: ['roboto'],
+    },
     extend: {},
   },
   plugins: [
